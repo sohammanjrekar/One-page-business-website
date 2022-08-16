ef654c25aca3c7a65158a68018d9d2b812df4362
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-08-16 21:51:20 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 const express =require("express");
 const path = require("path");
+const hbs =require("hbs")
 require("./db/conn");
 const app=express();
 const port=process.env.PORT || 3000;
 
 //setting paths
 const static_path=path.join("__dirname","../public");
+const templates_path=path.join("__dirname","../templates/views");
+const partials_path=path.join("__dirname","../templates/partials");
 //middleware
 app.use('/css',express.static(path.join(__dirname,"../node_modules/bootstrap/dist/css")))
 app.use('/js',express.static(path.join(__dirname,"../node_modules/bootstrap/dist/js")))
 app.use('/jq',express.static(path.join(__dirname,"../node_modules/jquery/dist")))
 app.use(express.static(static_path))
-
-
 app.set("view engine","hbs");
+app.set("views",templates_path);
+hbs.registerPartials(partials_path);
+
+
+
 //routing
 // app.get(path,callback)
 
